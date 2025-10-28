01ea74d40401b0c4e1b3284266a471d5abae1ea7
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-10-28 11:29:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((res) => {
+	return res.json();
+})
+.then((data) => {
+	return data.json();
+})
+.catch((log) => {
+	return log.json();
+})
