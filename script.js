87c7588e5f9a7670369786cc69c97ba05216bd70
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-10-29 12:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json()) // convert to JSON
   .then((data) => {
     const userList = document.getElementById("userList");
-
-    // Loop through each user and display name + email
     data.forEach((user) => {
       const div = document.createElement("div");
-      div.innerHTML = <strong>${user.name}</strong> — ${user.email};
+      div.innerHTML = `${user.name} — ${user.email}`;
       userList.appendChild(div);
     });
   })
